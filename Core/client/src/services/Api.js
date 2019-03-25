cdc15821357233ac2d84a5dac30814a63fd3add6
Bryce Remick <bryceremick@gmail.com>
2019-03-24 19:14:17 -0700

    Update Api.js

--- Core/client/src/services/Api.js
+++ Core/client/src/services/Api.js
@@ -1,15 +1,15 @@
 import axios from 'axios'
 
-export default() => {
+/*export default() => {
   return axios.create({
     baseURL: `http://localhost:3000`
   })
-}
-/*export default() => {
-  return axios.create({
-    baseURL: `http://10.62.82.171:8081`
-  })
 }*/
+export default() => {
+  return axios.create({
+    baseURL: `https://flyx-web-hosted.herokuapp.com/`
+  })
+}
 /*export default() => {
   return axios.create({
     baseURL: `https://abounding-smelt.glitch.me`
