0cafb18b82ac1cde0ecbbeccb0e31bc78af2d184
Bryce Remick <bryceremick@gmail.com>
2019-04-08 19:46:46 -0700

    User Account Related Features
    Implemented:
    -User Accounts (register/sign in/sign out)
    -Vuex (store)
    -Router
    -Navigation Guards
    -Persistence
    -Firebase functions
    
    Co-Authored-By: zdayscott <zdayscott@gmail.com>

--- Core/client/src/App.vue
+++ Core/client/src/App.vue
@@ -5,7 +5,23 @@
 </template>
 
 <script>
+import firebase from 'firebase/app';
+import'firebase/auth';
+
 export default {
   name: "App",
+  mounted() {
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+       this.$store.commit("initUser", user);
+       this.$router.push('/app');
+        console.log("user is logged in");
+      } else {
+        this.$store.commit("initUser", null);
+        this.$router.push('/');
+        console.log("user is logged out");
+      }
+    });
+  }
 };
 </script>
