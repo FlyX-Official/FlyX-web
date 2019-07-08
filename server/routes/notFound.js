109ce29a495f357c0a930fc9afda76c79d50f6c4
Bryce Remick <bryceremick@gmail.com>
2019-07-08 12:26:30 -0700

    removed unnecesary files/folders

--- server/routes/notFound.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('ERROR 404 - Page not found');
-});
-module.exports = router;
