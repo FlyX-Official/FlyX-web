0c4b91f9a2f3c8fd959e4379ee609766891dbaf0
Bryce Remick <work.bryceremick@gmail.com>
2019-11-22 16:37:24 -0800

    bumped dependecy versions

--- Core/client/src/services/Api.js
+++ Core/client/src/services/Api.js
@@ -1,12 +1,12 @@
 import axios from 'axios'
 
-export default() => {
-  return axios.create({
-    baseURL: `http://localhost:3000`
-  })
-}
 // export default() => {
 //   return axios.create({
-//     baseURL: `https://flyx-server.herokuapp.com/`
+//     baseURL: `http://localhost:3000`
 //   })
 // }
+export default() => {
+  return axios.create({
+    baseURL: `https://flyx-server.herokuapp.com/`
+  })
+}
